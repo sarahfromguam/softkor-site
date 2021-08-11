3a4aedc3c21dcf3e10e5e3bb09180eeefb1a5a76
Sarah Wilkinson <sarahwilkinson@Sarahs-MacBook-Pro-3.local>
2021-08-11 12:30:56 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from datetime import date
+from flask import Flask, render_template, redirect, url_for, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
